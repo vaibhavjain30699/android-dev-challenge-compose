f29471edf16f2bb93d6b977473a225d951408273
Vaibhav Jain <vaibhavjain30699@gmail.com>
2021-03-01 13:45:58 +0530

    Completed the Puppies list

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,15 +15,31 @@
  */
 package com.example.androiddevchallenge
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.typography
+import java.time.format.TextStyle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,10 +54,48 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun MyApp(names : List<String> = List(1000){"Puppy $it"}) {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column {
+            for (name in names){
+                PuppyView(name = name)
+                Spacer(modifier = Modifier.height(2.dp))
+            }
+        }
     }
+}
+
+@Composable
+fun PuppyView(name : String){
+    Column (
+        modifier = Modifier
+            .padding(16.dp)
+            .background(MaterialTheme.colors.background)
+        ) {
+        Image(
+            painter = painterResource(id = R.mipmap.puppy),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(4.dp)
+                .height(150.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(8.dp)),
+            contentScale = ContentScale.FillBounds
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        TextBox(label = name,modifier = Modifier.fillMaxWidth())
+//        TextBox(label = "2.5 yrs", modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun TextBox(label : String,modifier: Modifier){
+    Text(
+        text = label,
+        style = typography.body1,
+        textAlign = TextAlign.Center,
+        modifier = modifier,
+    )
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
@@ -52,10 +106,10 @@ fun LightPreview() {
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
-    }
-}
+//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun DarkPreview() {
+//    MyTheme(darkTheme = true) {
+//        MyApp()
+//    }
+//}
